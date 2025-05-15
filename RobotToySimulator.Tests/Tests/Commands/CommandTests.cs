781816ac8aabf69682f052643c321f83ae5cdfa2
rosegeni7 <rosminskhan777@gmail.com>
2025-05-15 05:31:16 +0530

    RobotToySimulator - Test

---
+++ RobotToySimulator.Tests/Tests/Commands/CommandTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using NUnit.Framework;
+using RobotToySimulator.Commands;
+using RobotToySimulator.Models;
+using NUnit.Framework.Legacy;
+
+namespace RobotToySimulator.Tests
+{
+    [TestFixture]
+    public class CommandTests
+    {
+        private Robot robot;
+
+        [SetUp]
+        public void SetUp()
+        {
+            robot = new Robot();
+        }
+
+        [Test]
+        public void PlaceCommand_Places_Correctly()
+        {
+            var cmd = new PlaceCommand(1, 2, Direction.SOUTH);
+            cmd.Execute(robot);
+            ClassicAssert.IsTrue(robot.IsPlaced);
+            ClassicAssert.AreEqual(0, robot.Position.X);
+            ClassicAssert.AreEqual(0, robot.Position.Y);
+            ClassicAssert.AreEqual(Direction.SOUTH, robot.Direction);
+        }       
+
+        [Test]
+        public void MoveCommand_Moves_Robot()
+        {
+            var place = new PlaceCommand(0, 0, Direction.EAST);
+            place.Execute(robot);
+
+            var move = new MoveCommand();
+            move.Execute(robot);           
+
+            ClassicAssert.AreEqual(1, robot.Position.X);
+            ClassicAssert.AreEqual(0, robot.Position.Y);
+        }
+
+        [Test]
+        public void LeftCommand_Turns_Left()
+        {
+            var place = new PlaceCommand(0, 0, Direction.NORTH);
+            place.Execute(robot);
+
+            var left = new LeftCommand();
+            left.Execute(robot);
+
+            ClassicAssert.AreEqual(Direction.WEST, robot.Direction);
+        }
+
+        [Test]
+        public void RightCommand_Turns_Right()
+        {
+            var place = new PlaceCommand(0, 0, Direction.NORTH);
+            place.Execute(robot);
+
+            var right = new RightCommand();
+            right.Execute(robot);
+
+            ClassicAssert.AreEqual(Direction.EAST, robot.Direction);
+        }
+
+        [Test]
+        public void ReportCommand_Prints_Correct_Output()
+        {
+            var place = new PlaceCommand(2, 3, Direction.WEST);
+            place.Execute(robot);
+
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var report = new ReportCommand();
+            report.Execute(robot);
+
+            var output = writer.ToString().Trim();
+            ClassicAssert.AreEqual("Output is 0,0,WEST", output);
+        }
+    }
+}
---
+++ RobotToySimulator.Tests/Tests/Models/RobotTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RobotToySimulator.Models;
+using RobotToySimulator.Commands;
+using NUnit.Framework.Legacy;
+
+namespace RobotToySimulator.Tests
+{
+    [TestFixture]
+    public class RobotTests
+    {
+        private Robot robot;
+
+        [SetUp]
+        public void SetUp()
+        {
+            robot = new Robot();
+        }
+
+        [Test]
+        public void Robot_Skip_Move_Before_Placement()
+        {
+            robot.Move();
+            robot.Report(); // Should not output anything
+            ClassicAssert.IsFalse(robot.IsPlaced);
+        }
+
+        [Test]
+        public void Place_Sets_Valid_Position()
+        {
+            robot.Place(1, 2, Direction.EAST);
+            ClassicAssert.IsTrue(robot.IsPlaced);
+            ClassicAssert.AreEqual(1, robot.Position.X);
+            ClassicAssert.AreEqual(2, robot.Position.Y);
+            ClassicAssert.AreEqual(Direction.EAST, robot.Direction);
+        }
+
+        // Move - Direction increment 
+        [Test]
+        public void Move_Update_Position_Correctly()
+        {
+            robot.Place(0, 0, Direction.NORTH);
+            robot.Move();
+            ClassicAssert.AreEqual(0, robot.Position.X);
+            ClassicAssert.AreEqual(1, robot.Position.Y);
+        }
+      
+        // Move Left Direction 
+        [Test]
+        public void Left_Rotates_Correctly()
+        {
+            robot.Place(0, 0, Direction.NORTH);
+            robot.Left();
+            ClassicAssert.AreEqual(Direction.WEST, robot.Direction);
+        }
+
+        // Move Right Direction 
+        [Test]
+        public void Right_Rotates_Correctly()
+        {
+            robot.Place(0, 0, Direction.NORTH);
+            robot.Right();
+            ClassicAssert.AreEqual(Direction.EAST, robot.Direction);
+        }
+      
+    }
+}
+
+
